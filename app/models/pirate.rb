725059093e59c38e86b41cf907590e8e3724d0f2
mikeisfake <mikescooper@icloud.com>
2019-11-19 21:59:08 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def initialize(props)
+    @name = props[:name]
+    @weight = props[:weight]
+    @height = props[:height]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+
+  attr_accessor :name, :type, :booty
+
+  def initialize(props)
+    @name = props[:name]
+    @type = props[:type]
+    @booty = props[:booty]
+    @@all << self
+  end
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
+
 end
